97c260d1341aa9cf0f446a6a37f62c666c069f6c
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-08-05 23:06:49 -0300

    test with Robson

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -3,7 +3,9 @@ package com.devsuperior.dsmovie.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmovie.entities.Movie;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>{
 
 }
